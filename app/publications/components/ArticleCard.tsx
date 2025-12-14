fb68136497288c1cf356db8f08265bfa75388821
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-12-14 20:47:36 +0800

    Fix: ArticleCard uses article as parameter instead of fetching data

--- app/publications/components/ArticleCard.tsx
+++ app/publications/components/ArticleCard.tsx
@@ -1,50 +1,36 @@
 import Link from 'next/link';
-import {getArticles} from '../data';
-
 import { Instrument_Sans } from "next/font/google";
 import { Inter } from "next/font/google";
-const instrumentSans = Instrument_Sans({
-  subsets: ["latin"],
-});
-const inter = Inter({
-  subsets: ["latin"],
-});
 
-export default async function ArticleCard({ id, variant = "internal" }) {
+const instrumentSans = Instrument_Sans({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
-  // Get article based on variant (internal/external) and id
-  const {internalPubs, externalPubs} = await getArticles();
-  const article = variant === "internal"
-    ? internalPubs.find((art) => art.id === id)
-    : externalPubs.find((art) => art.id === id);
-
-  if (!article) {
-    return <div>Article not found</div>;
-  }
-
-  // Get article metadata & slug
-  const {title, author, datePublished, thumbnail, authorDP} = article;
-  const link = 'articleLink' in article
-    ? article.articleLink
-    : `/publications/${title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '')}`;
+export default function ArticleCard({ article, variant = "internal" }) {
+  
+  // Get article details
+  const { title, author, datePublished, thumbnail, authorDP } = article;
   const text = 'content' in article ? article.content : article.summary;
   const date = new Date(datePublished).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
   });
+  
+  // Generate Slug or use Link
+  const link = 'articleLink' in article
+    ? article.articleLink
+    : `/publications/${title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '')}`;
 
-  // Main Card Component
   const CardContent = () => {
     return (
         <div className={`
-        overflow-hidden grid grid-cols-[1fr_1.75fr] gap-2 shadow-lg shadow-black/20
+        overflow-hidden grid grid-cols-[1fr_1.75fr] gap-2 sm:shadow-lg sm:shadow-black/20
         sm:grid-cols-1 sm:grid-rows-[1fr_1fr] sm:min-h-[460px] sm:bg-[#EEE7DC] sm:rounded-2xl hover:bg-[#DDD0BC] transition-all duration-300 cursor-pointer`}>
           <img src={thumbnail} alt="" className={`aspect-video object-cover h-full rounded-lg sm:rounded-t-2xl sm:rounded-b-[0]`} />
           <section className={`p-2 sm:px-6 sm:py-5 sm:flex sm:flex-col sm:justify-between`}>
             <div className={`flex flex-col gap-1 sm:gap-2`}>
               <div className={`flex items-center gap-2`}>
-                <img src="./assets/calendar.svg" alt="" className={`filter invert w-[12px]`} />
+                <img src="/assets/calendar.svg" alt="" className={`filter invert w-[12px]`} />
                 <p className={`${inter.className} text-[#373F2A] text-xs`}>{date}</p>
               </div>
               <h4 className={`${instrumentSans.className} font-semibold text-sm line-clamp-2 sm:text-base`}>
@@ -69,7 +55,6 @@ export default async function ArticleCard({ id, variant = "internal" }) {
     );
   }
 
-  // If external, use articleLink; if internal, link to internal route
   if (variant === "external") {
     return ( 
       <a href={link} target='_blank'>
